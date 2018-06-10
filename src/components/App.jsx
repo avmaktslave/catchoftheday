d228468abb0b86f8ba835e39325be796ab1b91ff
Alexey Kaydash <4tharraofdagon@gmail.com>
2018-06-10 16:14:36 +0300

    add fish component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,55 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
-  state = {};
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set tha vew fishes object to state
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="John Wick is cool" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Order extends Component {
   render() {
-    return <div className="order">Order!!!</div>;
+    return (
+      <div className="order">
+        <h2>Order</h2>
+      </div>
+    );
   }
 }
 
